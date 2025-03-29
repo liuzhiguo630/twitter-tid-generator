72a2bb1af2709c64fbb7dba0c62991e0813e570d
liuzhiguo630 <410915218@qq.com>
2025-03-29 16:18:16 +0800

    feat: first time fix (not working, missing frametime)

--- payload/payload.go
+++ payload/payload.go
@@ -12,7 +12,6 @@ import (
 
 const totalTime = 4096.0
 
-// GenerateHeader - generates an x-transaction-id header
 func GenerateHeader(path, method, key string, frames [][][]int) string {
 	keyBytes := []int{}
 	key = atob(key)
@@ -32,7 +31,7 @@ func GenerateHeader(path, method, key string, frames [][][]int) string {
 	row = row[7:]
 	curves := [4]float64{}
 	for i := 0; i < len(row); i++ {
-		curves[i] = a(float64(row[i]), b(i), 1.0)
+		curves[i] = toFixed(a(float64(row[i]), b(i), 1.0), 2)
 	}
 	c := &cubic{Curves: curves}
 	val := c.getValue(targetTime)
@@ -41,17 +40,30 @@ func GenerateHeader(path, method, key string, frames [][][]int) string {
 	matrix := convertRotationToMatrix(rotation[0])
 	strArr := []string{}
 	for i := 0; i < len(color)-1; i++ {
-		strArr = append(strArr, hex.EncodeToString([]byte{byte(math.Round(color[i]))}))
+		if color[i] > 0 {
+			roundedColor := math.Round(color[i])
+			if roundedColor >= 256 {
+				strArr = append(strArr, "ff")
+			} else {
+				strArr = append(strArr, strings.TrimPrefix(hex.EncodeToString([]byte{byte(roundedColor)}), "0"))
+			}
+		} else {
+			strArr = append(strArr, "0")
+		}
 	}
 	for i := 0; i < len(matrix)-2; i++ {
 		rounded := toFixed(matrix[i], 2)
-		if rounded < 0 {
-			rounded = -rounded
+		if rounded == float64(1) {
+			strArr = append(strArr, "1")
+		} else if rounded == float64(0) {
+			strArr = append(strArr, "0")
+		} else {
+			strArr = append(strArr, "0"+strings.ToLower(floatToHex(rounded)[1:]))
 		}
-		strArr = append(strArr, "0"+strings.ToLower(floatToHex(rounded)[1:]))
 	}
 	strArr = append(strArr, "0", "0")
-	hash := sha256.Sum256([]byte(fmt.Sprintf(`%s!%s!%vbird%s`, method, path, timeNow, strings.Join(strArr, ""))))
+	var keyWord = "obfiowerehiring"
+	hash := sha256.Sum256([]byte(fmt.Sprintf(`%s!%s!%v%s%s`, method, path, timeNow, keyWord, strings.Join(strArr, ""))))
 	hashBytes := []int{}
 	for i := 0; i < len(hash)-16; i++ {
 		hashBytes = append(hashBytes, int(hash[i]))
@@ -62,6 +74,8 @@ func GenerateHeader(path, method, key string, frames [][][]int) string {
 	bytes = append(bytes, keyBytes...)
 	bytes = append(bytes, timeNowBytes...)
 	bytes = append(bytes, hashBytes...)
+	var ADDITIONAL_RANDOM_NUMBER = 3
+	bytes = append(bytes, ADDITIONAL_RANDOM_NUMBER)
 	bytes = append(bytes, 1)
 	out := []byte{}
 	for i := 0; i < len(bytes); i++ {
